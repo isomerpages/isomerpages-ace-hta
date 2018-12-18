632415aaabb9061e1a59fc86b3fbb59329614649
Preston <prestonlimlianjie@gmail.com>
2018-12-18 14:24:26 +0800

    tries extracting the env vars from process.env

--- misc/travis-script.js
+++ misc/travis-script.js
@@ -5,20 +5,27 @@ const axios = require('axios')
 // If so, the function breaks out of the loop and makes an API call to purge the CDN cache.
 
 async function purgeCacheIfDeployed() {
-  console.log('In purgeCacheIfDeployed')
-  let deploySuccess = false
-  while (!deploySuccess) {
-    deploySuccess = await checkDeployState()
-  }
+  try {
+    console.log('In purgeCacheIfDeployed')
+    let deploySuccess = false
+    while (!deploySuccess) {
+      deploySuccess = await checkDeployState()
+    }
 
-  let purgeSuccess = false 
-  while (!purgeSuccess) {
-    purgeSuccess = await purgeCache()  
+    let purgeSuccess = false 
+    while (!purgeSuccess) {
+      purgeSuccess = await purgeCache()  
+    }
+  } catch (err) {
+    console.log(err)
   }
 }
 
 async function checkDeployState() {
   try {
+    let NETLIFY_SITE_ID = process.env.NETLIFY_SITE_ID
+    let NETLIFY_ACCESS_TOKEN = process.env.NETLIFY_ACCESS_TOKEN
+
     console.log('Checking if site has been deployed successfully')
     let resp = await axios.get(`https://api.netlify.com/api/v1/sites/${NETLIFY_SITE_ID}/deploys?access_token=${NETLIFY_ACCESS_TOKEN}`)
 
@@ -36,6 +43,9 @@ async function checkDeployState() {
 
 async function purgeCache() {
   try {
+    let KEYCDN_API_KEY = process.env.KEYCDN_API_KEY
+    let KEYCDN_ZONE_ID = process.env.KEYCDN_ZONE_ID
+    
     console.log('Purging cache')
     let resp = await axios.get(`https://${KEYCDN_API_KEY}@api.keycdn.com/zones/purge/${KEYCDN_ZONE_ID}.json`)
     if (resp.data.status === 200) {
